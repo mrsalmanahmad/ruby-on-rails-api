7a940fee2f6f6747e50d0a797b64881d9f0a4db0
mrsalmanahmad <salmanahmad3333@gmail.com>
2019-11-09 23:43:42 +0500

    added authentication

--- todos-api/config/routes.rb
+++ todos-api/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :todos do
   	resources :items
